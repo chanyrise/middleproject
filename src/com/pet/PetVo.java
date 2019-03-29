edd5f9342cf0c7e98ad90a716d4928a29c396eb5
chanyrise <44486518+chanyrise@users.noreply.github.com>
2019-03-29 23:16:18 +0900

    Add files via upload
    birthday by sunmin to birthdate by jaechan

--- src/com/pet/PetVo.java
+++ src/com/pet/PetVo.java
@@ -2,21 +2,22 @@ package com.pet;
 
 import java.sql.Date;
 
-//id, p_name, p_sex, p_size, p_birthdate
 public class PetVo {
-	private String id;
+	
 	private String p_name;
 	private String p_sex;
 	private String p_size;
 	private Date p_birthdate;
 	public PetVo() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
+	public PetVo(String p_name, String p_sex, String size, Date p_birthdate) {
+		super();
+		this.p_name = p_name;
+		this.p_sex = p_sex;
+		this.p_size = size;
+		this.p_birthdate = p_birthdate;
 	}
 	public String getP_name() {
 		return p_name;
@@ -42,5 +43,12 @@ public class PetVo {
 	public void setP_birthdate(Date p_birthdate) {
 		this.p_birthdate = p_birthdate;
 	}
+	@Override
+	public String toString() {
+		return "PetVo [p_name=" + p_name + ", p_sex=" + p_sex + ", p_size=" + p_size + ", p_birthdate=" + p_birthdate + "]";
+	}
+	
+
+	
 	
 }
