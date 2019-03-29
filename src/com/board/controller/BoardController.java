da91a2697fad59e1142147717327f2007799e490
chanyrise <44486518+chanyrise@users.noreply.github.com>
2019-03-29 23:56:38 +0900

    Add files via upload
    1. getAll구현 완료하였고
    2.
    BoardBiz.java파일에
    public BoardVo readContent(String no) {
    return boardDaoImpl.readContent(no);
    }
    구현했고,
    3.
    BoardDaoImpl.java에 
    @Override
    public BoardVo readContent(String no)
    구현했습니다.
    4.
    Controller에 readBoardList구현중이며
    5.
    index.jsp파일에 게시판 전체출력, 게시물 상세페이지 구현 중입니다.
    6.

--- src/com/board/controller/BoardController.java
+++ src/com/board/controller/BoardController.java
@@ -1,7 +1,31 @@
 package com.board.controller;
 
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.board.biz.BoardBiz;
+import com.board.vo.BoardVo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javax.activation.CommandMap;
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.board.biz.BoardBiz;
 
@@ -9,5 +33,31 @@ import com.board.biz.BoardBiz;
 public class BoardController {
 
 	@Autowired
-	BoardBiz boardBiz;
+	private BoardBiz boardBiz;
+
+	@RequestMapping(value = "board.do", method = RequestMethod.GET)
+	public ModelAndView openBoardList() throws Exception {
+		List<BoardVo> list = boardBiz.getAll();
+
+		ModelAndView mav = new ModelAndView("/View/Board/Board");// 이동할 페이지 지정
+		mav.addObject("list",list ); // 데이터 저장
+		return mav; // 페이지 이동(출력)
+	}
+
+	@RequestMapping(value = "boardcontent.do", method = RequestMethod.GET)
+	public ModelAndView insertBoardList(BoardVo vo) throws Exception{
+		int result = boardBiz.getInsert(vo);
+		ModelAndView mav = new ModelAndView("/View/Board/BoardPost"); //이동할 페이지 지정
+		return mav;
+			
+	}
+	
+	@RequestMapping(value = "readcontent.do", method = RequestMethod.GET)
+	public ModelAndView readBoardList(String no) throws Exception{
+		BoardVo result = boardBiz.readContent(no);
+		
+		ModelAndView mav = new ModelAndView("/View/Board/BoardContent"); //이동할 페이지 지정
+		mav.addObject("result", result);
+		return mav;
+	}
 }
